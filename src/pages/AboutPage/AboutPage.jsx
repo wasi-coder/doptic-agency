afa4bf72a333150dde2ad01be9a7ea8e85d832d7
WASI AL SAFA <2103058@student.ruet.ac.bd>
2025-12-20 21:39:15 +0600

    made the about page partner section ahero section and the banner section mostly

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import about_hero from './about_page_section/about_hero'  
+import AboutHero from './about_page_section/about_hero'
+import AboutBanner from './about_page_section/about_banner'
+import AboutPartner from './about_page_section/about_partner'
 
 const AboutPage = () => {
   return (
-    <div className="about-page">
-      <h1>About Us</h1>
+    <>
+     <AboutHero/>
+     <AboutBanner/>
+     <AboutPartner/>
       
-    </div>
+    </>
   )
 }
 
--- src/pages/HomePage/homePage.jsx
+++ src/pages/HomePage/homePage.jsx
@@ -5,8 +5,7 @@ import Services from './sections/Services'
 import Reviews from './sections/Reviews'
 import Blog from './sections/Blog'
 import FaqSection from './sections/Faq'
-import { CallToActionSection } from '../../components/Footer'
-import Footer from '../../components/Footer'
+
 
 const Home = () => {
   return (
