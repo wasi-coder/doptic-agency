a7b6051dfebfbd7ae95e5c8dcfef90030624cfb3
wasi-coder <wasialsafa025@gmail.com>
2025-12-23 12:49:41 +0600

    made changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'; // <--- 1. Import 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/doptic-agency/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"), // <--- 2. Add the alias mapping
